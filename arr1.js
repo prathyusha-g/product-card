5b72ad563b7f71bbfc5a0c2d5c61582b92aebda1
prathyusha-g <45353578+prathyusha-g@users.noreply.github.com>
2021-01-29 11:49:42 +0530

    Update arr1.js

--- arr1.js
+++ arr1.js
@@ -1,19 +1,9 @@
-/*var studentid=[1,2,3,4,5,6];
-console.log(studentid);
 
-for(var a=0;a<studentid.length;a++){
-console.log(studentid[a]);
-}*/
 
 var value=[11,22,33,"hi","hello","bye",11.2,12.3,13.4];
 console.log(value);
 
-/*for(var x=0;x<value.length;x++){
-	if (typeof value[x]=="string"){
-		console.log(value[x])
 
-	}
-	}*/
 function abc(value){
 	var res = value(22);
 	console.log(res)
